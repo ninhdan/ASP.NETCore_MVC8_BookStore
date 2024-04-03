70f052f82d554b213b186a3657b169f2c4bd320a
Nguyen Hoa Ninh Dan <dh52007049@student.stu.edu.vn>
2024-04-03 17:57:32 +0700

    Update

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -13,6 +13,18 @@ namespace Bulky.DataAccess.Data
         {
 
         }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+
+
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
@@ -147,13 +159,6 @@ namespace Bulky.DataAccess.Data
                 );
         }
 
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Product> Products { get; set; }
-        public DbSet<Company> Companies { get; set; }
-        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
-        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
-        public DbSet<OrderDetail> OrderDetails { get; set; }
-        public DbSet<OrderHeader> OrderHeaders { get; set; }
-        public DbSet<ProductImage> ProductImages { get; set; }
+        
     }
 }
